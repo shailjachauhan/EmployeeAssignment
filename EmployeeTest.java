7190be2c3cfb2628ceb7d3f2ff0be8131676ad9f
Shailja Chauhan <109363824+shailjachauhan@users.noreply.github.com>
2023-08-23 10:40:57 +0530

    Update EmployeeTest.java

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -1,7 +1,7 @@
 public class EmployeeTest {
     public static void main(String[] args) {
         // Creating an Employee object using the constructor
-        Employee employee = new Employee("John Doe", 30, "Manager", 50000.0);
+        Employee employee = new Employee("Shailja Chauhan", 24, "Software Developer", 90000.0);
         
         // Displaying employee information
         System.out.println("Employee Information:");
